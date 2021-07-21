39cd24bd091dd97a4b7ab321a8a6c59e836611bb
Drew DeVault <sir@cmpwn.com>
2021-07-21 09:36:26 +0200

    auth: allow /query/metrics w/o authentication

--- auth/middleware.go
+++ auth/middleware.go
@@ -688,7 +688,8 @@ func Middleware(conf ini.File, apiconf string) func(http.Handler) http.Handler {
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if !strings.HasPrefix(r.URL.Path, "/query") {
+			if !strings.HasPrefix(r.URL.Path, "/query") ||
+				r.URL.Path == "/query/metrics" {
 				next.ServeHTTP(w, r)
 				return
 			}
